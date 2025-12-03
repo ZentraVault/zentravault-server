fdd9f56a0f061a1e28f90156b82b7907096456b9
Mathéo Dalesme-Petitalot <matheo.dalesme-petitalot@epitech.eu>
2025-12-03 15:15:02 +0100

    Updated model.go
    Updated the User struct to make the attributes accessible.

--- src/user/model.go
+++ src/user/model.go
@@ -1,15 +1,12 @@
 package user
 
-import "go.mongodb.org/mongo-driver/bson/primitive"
-
 type User struct {
-	objectid primitive.ObjectID
-	username string
-	email    string
-	password string
-	token    string
-	id       string
-	status   string
-	friends  []string
-	groups   []string
+	Username string
+	Email    string
+	Password string
+	Token    string
+	ID       string
+	Status   string
+	Friends  []string
+	Groups   []string
 }
